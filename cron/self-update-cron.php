4ff8f08e7dfb71126ba66460754628fd886edf80
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-15 16:15:23 +0200

    do auto updates

--- cron/self-update-cron.php
+++ cron/self-update-cron.php
@@ -29,9 +29,49 @@ function fxwp_self_update()
 
         // Compare the version from the remote config with the current version
         if (version_compare($remote_version, $current_version, '>')) {
+            // The remote version is newer, perform self-update
 
+            // Define the URL of the GitHub repository's ZIP archive
+            $repo_url = 'https://github.com/ziegenhagel/faktorxwp/archive/main.zip';
 
+            // Define the directory where the plugin files are located
+            $plugin_directory = plugin_dir_path(__FILE__);
 
+            // Define the path of the ZIP archive
+            $zip_path = $plugin_directory . 'temp.zip';
+
+            // Initialize a new cURL session
+            $curl = curl_init();
+
+            // Set the cURL options
+            curl_setopt($curl, CURLOPT_URL, $repo_url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+
+            // Execute the cURL request and get the ZIP archive
+            $zip_content = curl_exec($curl);
+
+            // Close the cURL session
+            curl_close($curl);
+
+            // Save the ZIP archive
+            file_put_contents($zip_path, $zip_content);
+
+            // Open the ZIP archive
+            $zip = new ZipArchive();
+            if ($zip->open($zip_path)) {
+                // Extract the ZIP archive
+                $zip->extractTo($plugin_directory);
+                $zip->close();
+            }
+
+            // Delete the ZIP archive
+            unlink($zip_path);
+
+            // Log the self-update
+            error_log('Self-update performed successfully.');
         } else {
             // The plugin is already up to date
             error_log('No new update available.');
