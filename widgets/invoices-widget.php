6491d60d00597dac8f5c58c34257629f62a65622
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-15 18:08:42 +0200

    add ivnoices

---
+++ widgets/invoices-widget.php
@@ -0,0 +1,71 @@
+<?php
+
+function fxwp_invoices_widget()
+{
+    $invoices = get_option('fxwp_invoices', array());
+    $invoices = [
+        0 => [
+            'number' => '123',
+            'url' => 'https://p2.faktorxmensch.com/api/fxwp/invoices/123',
+            'created_at' => '2020-01-01',
+            'status' => 'paid',
+        ],
+        1 => [
+            'number' => '456',
+            'url' => 'https://p2.faktorxmensch.com/api/fxwp/invoices/456',
+            'created_at' => '2020-01-02',
+            'status' => 'paid',
+        ],
+    ];
+
+    if (empty($invoices)) {
+        echo '<p>' . esc_html__('Keine Rechnungen gefunden.', 'fxwp') . '</p>';
+        return;
+    }
+
+    echo '<table class="wp-list-table widefat fixed">';
+    echo '<thead><tr>';
+    echo '<th>' . esc_html__('Rechnung Nr.', 'fxwp') . '</th>';
+    echo '<th>' . esc_html__('Datum', 'fxwp') . '</th>';
+    echo '<th>' . esc_html__('Info', 'fxwp') . '</th>';
+    echo '</tr></thead>';
+
+    echo '<tbody>';
+    foreach ($invoices as $invoice) {
+        echo '<tr>';
+        echo '<td><a href="' . esc_url($invoice['url']) . '" target="_blank">' . esc_html($invoice['number']) . '</a></td>';
+        echo '<td>' . date('d.m.Y', strtotime($invoice['created_at'])) . '</td>';
+        echo '<td>' . fxwp_invoice_status($invoice['status']) . '</td>';
+        echo '</tr>';
+    }
+    echo '</tbody>';
+
+    echo '</table>';
+}
+
+function fxwp_invoice_status($status)
+{
+    switch ($status) {
+        case 'paid':
+            return 'Bezahlt';
+        case 'open':
+            return 'Offen';
+        case 'canceled':
+            return 'Storniert';
+        default:
+            return 'Unbekannt';
+    }
+}
+
+// Add dashboard widget
+function fxwp_register_invoices_widget()
+{
+    wp_add_dashboard_widget(
+        'fxwp_invoices_widget', // Widget slug.
+        'Invoices', // Title.
+        'fxwp_invoices_widget' // Display function.
+    );
+}
+
+
+add_action('wp_dashboard_setup', 'fxwp_register_invoices_widget');
