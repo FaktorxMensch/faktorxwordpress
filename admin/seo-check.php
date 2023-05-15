543bef4e4b4c494d731404132af05785aa8b8a64
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-15 16:46:08 +0200

    seo check

--- admin/seo-check.php
+++ admin/seo-check.php
@@ -2,8 +2,7 @@
 function fxwp_seo_check_page() {
     // Code to generate backup page
     echo '<h1>SEO Check</h1>';
-    echo '<p>Check your SEO</p>';
     // have todo for ema
-    echo '<p>TODO: Emaaaaaaaaaaa</p>';
+    echo '<p>TODO: Ema</p>';
 }
 
