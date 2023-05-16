0fecf910239f07b12c1c00d7b6be505376d1ddbf
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-16 15:10:35 +0200

    comment change

--- includes/config.php
+++ includes/config.php
@@ -12,5 +12,4 @@ define('FXWP_STORAGE_LIMIT', 20 * 1024 * 1024 * 1024); // 20GB
 define('FXWP_BACKUP_DAYS_SON', 1); // Backup jeden Tag
 define('FXWP_BACKUP_DAYS_FATHER', 7); // Backup jede Woche
 define('FXWP_BACKUP_DAYS_GRANDFATHER', 30); // Backup jeden Monat
-// how many grandfathers to keep
-define('FXWP_BACKUP_GRANDFATHERS', 2);
+define('FXWP_BACKUP_GRANDFATHERS', 2); // how many grandfathers to keep
