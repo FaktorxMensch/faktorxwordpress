4ec36fd23eba2df25fdd963d8e6c8cadb2f60a33
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-16 10:36:35 +0200

    seperate into mod

--- widgets/maintenance-mode-widget.php
+++ widgets/maintenance-mode-widget.php
@@ -53,29 +53,3 @@ function display_maintenance_mode_widget()
 }
 
 
-// Widget display callback function
-function display_maintenance_mode()
-{
-    if (!is_user_logged_in()) {
-        $maintenance_mode = get_option('maintenance_mode', 'none'); // 'none', 'coming_soon', 'maintenance_mode
-
-        if ($maintenance_mode == 'maintenance_mode') {
-            echo '<p>' . __('Wartungsmodus. Unsere Website ist vorübergehend nicht verfügbar.', 'text-domain') . '</p>';
-        } else if ($maintenance_mode == 'coming_soon') {
-            echo '<p>' . __('Demnächst verfügbar. Unsere Website befindet sich im Aufbau.', 'text-domain') . '</p>';
-        } else {
-            return;
-        }
-
-        die(); // Stop further execution
-    }
-}
-
-// Register the widget
-function register_maintenance_mode()
-{
-    add_action('template_redirect', 'display_maintenance_mode');
-
-}
-
-add_action('wp', 'register_maintenance_mode');
