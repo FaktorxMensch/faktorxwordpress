c0c6ed3aac983c2a19e03b4149e5408b51b93a59
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-17 12:24:58 +0200

    change backups to hourly

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,7 @@ define('FXWP_STORAGE_LIMIT', 20 * 1024 * 1024 * 1024); // 20GB
 
 // GVS Intervalle
 // wir wollen die letzten 24 stunden, dann von jedem tag der letzten woche, dann von jedem monat der letzten 2 monate
-define('FXWP_BACKUP_INTERVAL', 'daily'); // vs daily
+define('FXWP_BACKUP_INTERVAL', 'hourly'); // vs daily
 
 define('FXWP_BACKUP_DAYS_SON', 3); // keep hourly backups for the last X hours
 define('FXWP_BACKUP_DAYS_FATHER', 12); // keep daily backups for the last X days
