a09fcfe2ffccd1e36a524fdc0b1082b20e5d8989
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-18 11:31:29 +0200

    simple hide styles

--- mods/view-option-mod.php
+++ mods/view-option-mod.php
@@ -1,18 +1,23 @@
 <?php
 function fxwp_render_view_option()
 {
-    if (get_option('fxwp_view_option', 'einfach') === 'erweitert' || current_user_can('erweiterte_ansicht')) {
-        // Code to display in Erweiterte Ansicht or for users with erweiterte_ansicht capability
-        echo '<style>
-        .hide_simple, .hide_advanced {
+    // Code to display in Erweiterte Ansicht or for users with erweiterte_ansicht capability
+    // Admins
+    echo '<style>
+        #wp-admin-bar-comments,
+        #menu-comments, .hide_simple,#dashboard_activity, #dashboard_right_now, #dashboard_site_health,#dashboard_primary, #contextual-help-link-wrap 
+        {
             display:none !important;
         }
         </style>';
-        // ...
-    } else {
+    // ...
+    if (!get_option('fxwp_view_option', 'einfach') === 'erweitert' || current_user_can('erweiterte_ansicht')) {
         // Code to display in Einfache Ansicht
+        // Customers
         echo '<style>
-        .hide_simple {
+        .hide_simple, .hide_advanced, #menu-plugins, #menu-users, .plugin-title .row-actions.visible, a[href$="plugin-install.php"], 
+        #menu-appearance a[href="themes.php"].wp-first-item, a[href$="theme-install.php"], #menu-tools
+        {
             display:none !important;
         }
         </style>';
