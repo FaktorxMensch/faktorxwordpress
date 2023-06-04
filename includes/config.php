2fa722bdba8bb1f1850add057db7424aa54b468b
Dominik <dominik.ziegenhagel@gmail.com>
2023-06-04 20:00:45 +0200

    update config

--- includes/config.php
+++ includes/config.php
@@ -7,18 +7,11 @@ define('FXWP_VERSION', '0.2');
 // check if we are in a local environment
 if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false || strpos($_SERVER['HTTP_HOST'], '.local') !== false) {
     define('FXWP_LOCAL_ENV', true);
-} else {
-    define('FXWP_LOCAL_ENV', false);
-}
-
-//
-if(FXWP_LOCAL_ENV) {
     define('FXWP_API_URL', 'http://localhost:3000/api/fxwp');
 } else {
+    define('FXWP_LOCAL_ENV', false);
     define('FXWP_API_URL', 'https://p2.faktorxmensch.com/api/fxwp');
 }
-//define('FXWP_API_URL', 'https://p2.faktorxmensch.com/api/fxwp');
-//define('FXWP_API_URL', 'http://localhost:3000/api/fxwp');
 
 // Server Settings
 define('FXWP_STORAGE_LIMIT', 20 * 1024 * 1024 * 1024); // 20GB
