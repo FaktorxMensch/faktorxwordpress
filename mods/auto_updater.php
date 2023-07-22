d70246644d207ac64dd105724d89683327895f34
leubeem <emanuel@binder-leube.de>
2023-07-22 08:56:17 +0200

    Some improvements like up to date notice

--- mods/auto_updater.php
+++ mods/auto_updater.php
@@ -5,9 +5,12 @@ add_action( 'fxm_hourly_event', 'fxm_do_this_hourly' );
 
 if ( ! function_exists( 'fxm_do_this_hourly' ) ) {
 	function fxm_do_this_hourly() {
-		error_log( "=====================================================" );
+		error_log( "================  FXWP Plugin Update  ====================" );
 		error_log( "Local env: " . ( FXWP_LOCAL_ENV ? "true" : "false" ) );
-		error_log( "Plugin dir: " . FXWP_PLUGIN_DIR );
+		if (FXWP_LOCAL_ENV) {
+			error_log("Not checking for updates in local environment");
+			return;
+		}
 
 		$plugin_data = get_plugin_data( FXWP_PLUGIN_DIR . '/faktorxwordpress.php' );
 		$current_version = $plugin_data['Version'];
@@ -28,9 +31,14 @@ if ( ! function_exists( 'fxm_do_this_hourly' ) ) {
 			error_log("Current version: " . $current_version . " Latest version: " . $latest_version);
 
 			if ( version_compare( $current_version, $latest_version, '<' ) ) {
-				error_log( "fxm_care_do_update" );
+				//error_log( "fxm_care_do_update" );
 				// New update available, trigger the update process.
 				fxm_plugin_updater($latest_version_git);
+			} else {
+				error_log( "No update available" );
+				add_action( 'admin_notices', function (){
+					echo '<div class="notice notice-success is-dismissible"><p>Your plugin is up to date.</p></div>';
+				} );
 			}
 		}
 
@@ -57,7 +65,6 @@ function fxm_plugin_updater($latest_version_git) {
 	// Step 2: Check if the download was successful.
 	if ( ! is_wp_error( $temp_file ) ) {
 
-		error_log("WP_PLUGIN_DIR: " . WP_PLUGIN_DIR);
 		// Step 3: Unzip the downloaded file and overwrite the existing plugin files.
 		$unzip_result = unzip_file( $temp_file, WP_PLUGIN_DIR );
 
@@ -70,13 +77,8 @@ function fxm_plugin_updater($latest_version_git) {
 			error_log( "Error occurred while unzipping.");
 		} else {
 			// Successful update.
-			// You can do additional tasks here, like running database updates, etc.
-			// Optionally, you can update the plugin version in the database.
-
 
 			$extracted_root_folder = trailingslashit( WP_PLUGIN_DIR ) . basename(FXWP_PLUGIN_DIR )  . '-' . $latest_version;
-
-			error_log("Extracted root folder: " . $extracted_root_folder);
 
 			fxm_move_directory_contents( $extracted_root_folder, FXWP_PLUGIN_DIR );
 			fxm_recursive_delete( $extracted_root_folder );
@@ -98,7 +100,6 @@ function fxm_plugin_updater($latest_version_git) {
 function fxm_move_directory_contents( $src, $dest ) {
 	$files = glob( $src . '/*' );
 	foreach ( $files as $file ) {
-		error_log("Moving file: " . $file . " to: " . $dest . basename( $file ));
 		if ( is_file( $file ) ) {
 			$file_dest = $dest . '/' . basename( $file );
 			copy( $file, $file_dest );
