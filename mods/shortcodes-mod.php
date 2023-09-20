239c3839624b1b2b654a54a8c2f1076c15e13a53
leubeem <emanuel@binder-leube.de>
2023-09-20 14:27:58 +0200

    Added shortcode for inserting menus

--- mods/shortcodes-mod.php
+++ mods/shortcodes-mod.php
@@ -3,6 +3,49 @@ function fxwp_register_shortcodes()
 {
     $fxwp_shortcodes = get_option('fxwp_shortcodes', array());
 
+    // Check if the 'fxwp-menu' shortcode is already registered
+    $fxwp_menu_shortcode_exists = false;
+
+    foreach ($fxwp_shortcodes as $shortcode_data) {
+        if ($shortcode_data['tag'] === 'fxwp-menu') {
+            $fxwp_menu_shortcode_exists = true;
+            break;
+        }
+    }
+
+    if (!$fxwp_menu_shortcode_exists) {
+        // Define the attributes and code for the 'fxwp-menu' shortcode
+        $menu_shortcode_data = array(
+            'tag' => 'fxwp-menu',
+            'attributes' => array(
+                array('name' => 'name', 'description' => 'menu name', 'default' => ''),
+            ),
+            'description' => 'FXM menu Shortcode for adding menus into the page',
+            'code' => '<?php
+    $menu_name = $atts["name"];
+    $menu = wp_get_nav_menu_object($menu_name);
+
+    if ($menu) {
+        $menu_args = array(
+            "menu" => $menu_name,
+            "menu_class" => "extra-menu footer-menu list-none", // Customize the CSS class
+            "echo" => true // Display the menu immediately
+        );
+
+        wp_nav_menu($menu_args);
+    } else {
+        echo "Menu not found.";
+    }
+?>',
+        );
+
+        // Add the 'fxwp-menu' shortcode data to the options array
+        $fxwp_shortcodes[] = $menu_shortcode_data;
+
+        // Update the options in the database
+        update_option('fxwp_shortcodes', $fxwp_shortcodes);
+    }
+
     foreach ($fxwp_shortcodes as $shortcode_data) {
         $shortcode_tag = $shortcode_data['tag'];
         $attributes = $shortcode_data['attributes'];
