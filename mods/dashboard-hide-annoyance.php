e760dd466761f4db2528f986cda7440a959f0a1a
leubeem <emanuel@binder-leube.de>
2024-01-24 03:13:18 +0100

    Hid one annoying admin notice and added some custom footer parts

--- mods/dashboard-hide-annoyance.php
+++ mods/dashboard-hide-annoyance.php
@@ -22,3 +22,42 @@ function remove_dashboard_widgets () {
 		remove_meta_box('e-dashboard-overview', 'dashboard', 'normal', 'core');
 	}
 }
+
+function fxwp_hide_admin_notices_with_reviews() {
+
+	// This global object is used to store all plugins callbacks for different hooks
+	global $wp_filter;
+
+	// Here we define the strings that we don't want to appear in any messages
+	$forbidden_message_strings = [
+		'Essential Addons'
+	];
+
+	// Now we can loop over each of the admin_notice callbacks
+	foreach($wp_filter['admin_notices'] as $weight => $callbacks) {
+
+		foreach($callbacks as $name => $details) {
+
+			// Start an output buffer and call the callback
+			ob_start();
+			call_user_func($details['function']);
+			$message = ob_get_clean();
+
+			// Check if this contains our forbidden string
+			foreach($forbidden_message_strings as $forbidden_string) {
+				if(strpos($message, $forbidden_string) !== FALSE) {
+
+					// Found it - under this callback
+					$wp_filter['admin_notices']->remove_filter('admin_notices', $details['function'], $weight);
+
+					// admin notice with infos about the plugin for debugging
+					// echo '<div class="notice notice-info"><p>The annoying notice is: ' .print_r($details['function'], true). ', '.$weight.'</p></div>';
+				}
+			}
+
+		}
+
+	}
+}
+
+add_action('in_admin_header', 'fxwp_hide_admin_notices_with_reviews');
