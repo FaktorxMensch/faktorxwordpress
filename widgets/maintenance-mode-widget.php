5bcce9cc0cc6fad2d538cc6b6069fe6f0c6fcebc
leubeem <emanuel@binder-leube.de>
2024-02-06 18:50:20 +0100

    Changed behaviour to fix function 'open Project in p2'

--- widgets/maintenance-mode-widget.php
+++ widgets/maintenance-mode-widget.php
@@ -95,7 +95,7 @@ function fxwp_display_maintenance_mode_widget()
 			"title"=>"Open project in p2",
 			"type"=>"action",
 			"description"=>"Opens the associated project in p2 for faster access.",
-			"link"=>FXWP_P2_URL.'/project/'.(get_option('fxwp_customer')['_id']),
+			"link"=>FXWP_P2_URL.'/project/'.(get_option('fxwp_project')['_id']),
 			"disabled"=>false,
 		],
 /*		[
